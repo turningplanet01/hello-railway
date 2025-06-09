3fbdc98e5e609e8126fd0f861d8fe0bf902cb161
Saydy01 <turningplanet01@gmail.com>
2025-06-08 18:30:11 -0700

    os

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def hello():
-#     return "Hello, Railway!"
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0")
-
-
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS  # 解决跨域问题
 
